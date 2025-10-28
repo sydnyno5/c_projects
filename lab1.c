1ee89e976fe6f113a145c7cc62e52d7e8776dae9
sydnyno5 <sydneyvs007@gmail.com>
2025-10-28 17:35:32 +0700

    first commit

---
+++ lab1.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+void main(void){
+    // printf("hello world \n");
+    // printf("sydny pukpik");
+
+}
+// int pukpik =20;
+char name[5] = "Sydny";
---
+++ lab2.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(void){
+    int age=16;
+    char name[50]="Varisa Somvasoontara";
+    char gender = 'W';
+    float gpa=3.80;
+    printf("My name %s \n", name); //string is word s
+    printf("I'm %d \n", age); // decimal is int number d
+    printf("Grade %f \n", gpa); // float is decimal f
+    printf("Gender %c \n", gender); // char is c
+    // printf("")
+    return 0;
+}
