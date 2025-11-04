f5307f227bafe934093068e1c0909ecbf84c1219
sydnyno5 <sydneyvs007@gmail.com>
2025-11-04 17:41:09 +0700

    update

---
+++ lab3.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+int main(void){
+    float area;
+    int base;
+    int heigh;
+
+    printf ("Enter Base:");
+    scanf("%d", &base);
+
+    printf("Enter heigh :");
+    scanf("%d",&heigh);
+
+    area = 0.5*base*heigh;
+
+    // printf ("Base = %d \n", base);
+    // printf ("Heigh = %d \n", heigh);
+    // printf ("area = %f \n", area);
+    printf ("base =%d \nheigh =%d \n area = %.2f", base, heigh, area);
+    
+
+
+
+
+
+
+
+}
+
+    // printf ("Enter Base:");
+    // scanf("%d", &base);
+
+    // printf("Enter heigh :");
+    // scanf("%d",&heigh);
+
+    // area = 0.5*base*heigh;
+
+    // printf ("Base = %d \n", base);
+    // printf ("Heigh = %d \n", heigh);
+    // printf ("area = %f \n", area);
+    // printf ("base =%d \nheigh =%d \n area = %.2f", base, heigh, area);
---
+++ lab5.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main(void){
+    // float area;
+    // int base;
+    // int heigh;
+    float mass;
+    int weight = 47;
+    float height = 163;
+
+    height = height/100;
+    mass = weight/(height*height);
+    printf ("weight =%d \n height =%.0f \n mass = %.0f", weight, height*100, mass);
+
+
+
+
+
+
+
+}
